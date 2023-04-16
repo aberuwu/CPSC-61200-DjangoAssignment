9fa50909a5b2ef03301a1516141779067e75c957
Cesar I. Mendoza <51037899+aberuwu@users.noreply.github.com>
2023-04-16 15:36:10 -0500

    Add json response to view

--- DjangoDemo/MyDjangoProject/polls/views.py
+++ DjangoDemo/MyDjangoProject/polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world!")
+    helloWorld = {'Message': "Hello World"}
+    return JsonResponse(helloWorld)
